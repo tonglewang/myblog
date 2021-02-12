f21db3290760be1d85937d9adff0798068da97d8
tonglewang <18918079120@163.com>
2021-02-12 16:29:39 +0800

    fix the setting file:profile

--- myblog/settings.py
+++ myblog/settings.py
@@ -129,32 +129,14 @@ STATICFILES_DIRS = (
 
 if os.getcwd() == '/app':
     import dj_database_url
-
     DATABASES = {
         'default': dj_database_url.config(default='postgres://localhost')
     }
-
     # 让request.is_secure()承认X-Forwarded-Proto头
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
     # 只允许Heroku托管这个项目
     ALLOWED_HOSTS = ["https://tongleblog.herokuapp.com"]
     DEBUG = False
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-if os.getcwd() == '/app':    # 函数getcwd() ，它获取当前的工作目录 （当前运行的文件所在的目录）,在Heroku部署中，这个目录总是/app
-    import dj_database_url
-    DATABASES = {
-        'default': dj_database_url.config(default='postgres://localhost')
-    }
-    # 让request.is_secure()承认X-Forwarded-Proto头
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    # 支持所有的主机头（host header）
-    ALLOWED_HOSTS = ['*']
-    # 静态资产配置
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),)
 
 django_heroku.settings(locals())
--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -11,8 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from dj_static import Cling
+# from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+
+# from whitenoise.django import DjangoWhiteNoise
+#
+# application = DjangoWhiteNoise(get_wsgi_application())
