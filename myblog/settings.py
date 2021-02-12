d793a586f6b5fd8f96bdbff1350a3c00df9e9386
tonglewang <18918079120@163.com>
2021-02-12 19:31:23 +0800

    fix the setting file:profile

--- myblog/settings.py
+++ myblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v!!pwv2nac3m#a=#n%hv&3bvl@l(4iu$ce+h%5vj0&j%m_891-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
